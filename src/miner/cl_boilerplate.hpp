bde718ec95a3d2d494d5c9e94fb1111871ae1a6c
Yong Wen Chua <lawliet89@gmail.com>
2014-03-17 16:19:33 +0000

    Formatting issues

--- src/miner/cl_boilerplate.hpp
+++ src/miner/cl_boilerplate.hpp
@@ -36,17 +36,17 @@ void setupOpenCL(std::vector<cl::Platform> &platforms,
   if (platforms.size() == 0)
     throw std::runtime_error("No OpenCL platforms found.");
 
-  log -> Log(Log_Info, "[OpenCL] Found %d platforms", platforms.size());
+  log->Log(Log_Info, "[OpenCL] Found %d platforms", platforms.size());
   for (unsigned i = 0; i < platforms.size(); i++) {
     std::string vendor = platforms[0].getInfo<CL_PLATFORM_VENDOR>();
-    log -> Log(Log_Info, "[OpenCL]  Platform %i: %s", i, vendor.c_str());
+    log->Log(Log_Info, "[OpenCL]  Platform %i: %s", i, vendor.c_str());
   }
 
   int selectedPlatform = 0;
   if (getenv("HPCE_SELECT_PLATFORM")) {
     selectedPlatform = atoi(getenv("HPCE_SELECT_PLATFORM"));
   }
-  log -> Log(Log_Info, "[OpenCL] Choosing platform %d", selectedPlatform);
+  log->Log(Log_Info, "[OpenCL] Choosing platform %d", selectedPlatform);
   cl::Platform platform = platforms.at(selectedPlatform);
 
   platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
@@ -54,20 +54,31 @@ void setupOpenCL(std::vector<cl::Platform> &platforms,
     throw std::runtime_error("No opencl devices found.");
   }
 
-  log -> Log(Log_Info, "[OpenCL] Found %d devices", devices.size());
+  log->Log(Log_Info, "[OpenCL] Found %d devices", devices.size());
   for (unsigned i = 0; i < devices.size(); i++) {
     std::string name = devices[i].getInfo<CL_DEVICE_NAME>();
-    log -> Log(Log_Info, "[OpenCL]  Device %i: %s", i, name.c_str());
+    log->Log(Log_Info, "[OpenCL]  Device %i: %s", i, name.c_str());
   }
 
   int selectedDevice = 0;
   if (getenv("HPCE_SELECT_DEVICE")) {
     selectedDevice = atoi(getenv("HPCE_SELECT_DEVICE"));
   }
-  log -> Log(Log_Info, "[OpenCL] Choosing device %d", selectedDevice);
+  log->Log(Log_Info, "[OpenCL] Choosing device %d", selectedDevice);
   device = devices.at(selectedDevice);
 
-  context  = cl::Context(devices);
+  context = cl::Context(devices);
+
+  // Harvesting some information
+  size_t value;
+  device.getInfo(CL_DEVICE_MAX_COMPUTE_UNITS, &value);
+  log->Log(Log_Info, "[OpenCL] CL_DEVICE_MAX_COMPUTE_UNITS %d", value);
+  device.getInfo(CL_DEVICE_MAX_WORK_GROUP_SIZE, &value);
+  log->Log(Log_Info, "[OpenCL] CL_DEVICE_MAX_WORK_GROUP_SIZE %d", value);
+  device.getInfo(CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, &value);
+  log->Log(Log_Info, "[OpenCL] CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS %d", value);
+  device.getInfo(CL_DEVICE_MAX_WORK_ITEM_SIZES, &value);
+  log->Log(Log_Info, "[OpenCL] CL_DEVICE_MAX_WORK_ITEM_SIZES %d", value);
 }
 }; // namespace bitecoin
 #endif
